e05386da9e162cb2be3e3524830178acdab78fe4
alcmarfil <alcmarfil@gmail.com>
2026-04-25 13:46:48 +0800

    feat: functions needed for the game to work

---
+++ src/main/java/com/hueharvest/shared/GameState.java
@@ -0,0 +1,64 @@
+package com.hueharvest.shared;
+
+import java.io.Serializable;
+
+public class GameState implements Serializable {
+    public static final int GRID_SIZE = 30;
+    
+    // 0 = Neutral / Tile not yet taken , 1-4 = Player Colors
+    private final int[][] grid;
+    private int timeLeft; // in seconds
+    
+    public GameState() {
+        this.grid = new int[GRID_SIZE][GRID_SIZE];
+        this.timeLeft = 10;
+    }
+
+    public int[][] getGrid() {
+        return grid;
+    }
+
+    public void setTile(int x, int y, int playerId) {
+        if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+            grid[y][x] = playerId;
+        }
+    }
+
+    public int getTile(int x, int y) {
+        if (x >= 0 && x < GRID_SIZE && y >= 0 && y < GRID_SIZE) {
+            return grid[y][x];
+        }
+        return -1;
+    }
+
+    public int getTimeLeft() {
+        return timeLeft;
+    }
+
+    public void setTimeLeft(int timeLeft) {
+        this.timeLeft = timeLeft;
+    }
+
+    public void tick() {
+        if (timeLeft > 0) timeLeft--;
+    }
+
+    public void reset() {
+        for (int y = 0; y < GRID_SIZE; y++) {
+            for (int x = 0; x < GRID_SIZE; x++) {
+                grid[y][x] = 0;
+            }
+        }
+        this.timeLeft = 10; // change kapag final na
+    }
+
+    public int getTileCount(int playerId) {
+        int count = 0;
+        for (int[] row : grid) {
+            for (int tile : row) {
+                if (tile == playerId) count++;
+            }
+        }
+        return count;
+    }
+}
